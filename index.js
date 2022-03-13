3e9e1abe9bade011862928c4803cee1b4339b2df
Nicholas <mullally.nt@gmail.com>
2022-03-13 15:56:49 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+
+function add(a, b){
+    let res = a + b 
+    return res
+}
+
+function subtract(a, b){
+    let res = a - b
+    return res
+    
+}
+
+function multiply(a, b){
+    let res = a * b
+    return res
+
+}
+function divide(a,b){
+    let res = a / b
+    return res
+
+}
+function increment(n){
+    let res = ++n
+    return res
+
+}
+function decrement(n){
+    let res = --n
+    return res
+
+}
+
+function parseInt() {
+
+}
+function parseFloat(){
+
+}
+
+
+
+function makeInt(n){
+    parseInt("n")
+}
